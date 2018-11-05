484d73f7e1d8daba7c8850cfe47d05b14c0643d7
Cjarit <cjarit@gmail.com>
2018-11-05 17:36:01 +0700

    update meta tag

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,8 +27,52 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: 'description',
+              content:
+                'เว็บไซต์แฟนคลับของฟีฟ่าแห่งวง BNK48 ทำความรู้จักฟีฟ่าพร้อมทั้งช่องทางการติดตามต่างๆ',
+            },
+            {
+              name: 'keywords',
+              content:
+                'ฟีฟ่า, FifaBNK48, ฟีฟ่า BNK48, Fifa BNK48, Fifa, เด็กม่วง, สนามบอลของฟีฟ่า, สนามบอลของเด็กม่วง, BNK48, Fortune Cookie, คุ๊กกี้เสี่ยงทาย, ลูกทุ่ง',
+            },
+            {
+              name: 'url',
+              content:
+                'http://www.fifabnk48.com/',
+            },
+            {
+              name: 'og:title',
+              content:
+                'Fifa BNK48 Fansite : เว็บแฟนคลับฟีฟ่า BNK48',
+            },
+            {
+              name: 'og:image',
+              content:
+                '../images/intro-image01.jpg',
+            },
+            {
+              name: 'og:type',
+              content:
+                'profile',
+            },
+            {
+              name: 'og:url',
+              content:
+                'http://www.fifabnk48.com/',
+            },
+            {
+              name: 'og:site_name',
+              content:
+                'Fifa BNK48 Fansite',
+            },
+            {
+              name: 'og:description',
+              content:
+                'เว็บไซต์แฟนคลับของฟีฟ่าแห่งวง BNK48 ทำความรู้จักฟีฟ่าพร้อมทั้งช่องทางการติดตามต่างๆ',
+            },
+            
           ]}
           link={[
             { rel: 'shortcut icon', type: 'image/png', href: `${Favicon}` },
